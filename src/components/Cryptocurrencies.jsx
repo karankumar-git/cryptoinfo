17d26207169041c787eb6cc7cbd08e046aef3562
Karan Kumar <karankumarsonkaria@gmail.com>
2022-07-24 12:12:21 +0530

    navbar responsiveness

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
-
+import { BackTop } from 'antd';
 import { useGetCryptosQuery } from '../services/cryptoApi';
 import Loader from './Loader';
 
@@ -32,6 +32,7 @@ const Cryptocurrencies = ({ simplified }) => {
           />
         </div>
       )}
+      <BackTop style={{fontSize:"112px"}} />
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map((currency) => (
           <Col
